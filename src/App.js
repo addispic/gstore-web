6320453533e6545994291271e51d79255d4e1040
addispic <addispik@gmail.com>
2024-10-12 06:34:52 -0700

    changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 import React from 'react'
+import {Routes,Route, useLocation} from 'react-router-dom'
+
+
+// components
+// header
+import Header from './components/Header'
+
+// pages
+// home
+import Home from './pages/Home'
+// user
+import Users from './pages/users/Users'
 
 const App = () => {
+
+  // hooks
+  const pathname = useLocation().pathname
   return (
-    <div>App</div>
+    <div className='w-[100vw] h-[100vh]'>
+      {
+        !pathname?.startsWith("/users") && <Header />
+      }
+      <Routes>
+        {/* home */}
+        <Route path='/' element={<Home />}></Route>
+        {/* user */}
+        <Route path='/users' element={<Users />}></Route>
+      </Routes>
+    </div>
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {NavLink} from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <div>Header <NavLink to={'/users'}>Users</NavLink></div>
+  )
+}
+
+export default Header
